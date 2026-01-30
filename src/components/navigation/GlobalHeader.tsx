4e69ceac1d18c578c0880af8c9458d2e76fa461f
Smarter-Poker <admin@smarter.poker>
2026-01-30 01:28:13 -0600

    Revert "Remove box from diamond balance and improve styling"
    This reverts commit 6914d50bbb90bc65e4e9e676bfbd1f9d8179e270.

--- src/components/navigation/GlobalHeader.tsx
+++ src/components/navigation/GlobalHeader.tsx
@@ -168,7 +168,7 @@ export default function GlobalHeader({
                             }
                         }}
                     >
-                        <span><img src="/images/diamond.png" alt="Diamond" style={{ width: 28, height: 28, display: "inline-block", verticalAlign: "middle" }} /></span>
+                        <span>💎</span>
                         <span className={styles.statValue} title={diamonds.toLocaleString() + ' diamonds'}>
                             {isLoadingDiamonds ? '...' : showFullDiamonds ? diamonds.toLocaleString() : formatCompact(diamonds)}
                         </span>
